0c3f12d0974b3bd1b9cce284db6e57b6f33cd5de
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-02-02 23:58:59 +0000

    A simpler AssertEquals for Arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+const assertArraysEqual = function(actual, expected) {
+  let equalCondition;
+  if (Array.isArray(actual) && Array.isArray(expected)) {
+    equalCondition = eqArrays(actual, expected);
+  } else {
+    equalCondition = false;
+  }
+
+  if (equalCondition === true) {
+    return console.log(`👏 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`👎 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actual,expected) {
+  let equalCondition = true;
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        equalCondition = false;
+        break;
+      }
+    }
+  } else {
+    equalCondition = false;
+  }
+  return equalCondition;
+};

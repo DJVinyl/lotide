8f02275ded8893230eb77701df9e201695cac8d5
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-02-02 20:20:10 +0000

    added middle functionality

---
+++ middle.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  if (Array.isArray(actual) === true && Array.isArray(expected) === true) {
+    if (arrayEquals(actual,expected) === true) {
+      return console.log(`👏 Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      return console.log(`👎 Assertion failed: ${actual} !== ${expected}`);
+    }
+  } else {
+    if (actual === expected) {
+      return console.log(`👏 Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      return console.log(`👎 Assertion failed: ${actual} !== ${expected}`);
+    }
+  }
+};
+
+const middle = function(arr){
+  if (arr.length < 3) {
+    return [];
+  } else {
+    const middleArr = [];
+    if (arr.length % 2  === 0) {
+      middleArr.push(arr[(arr.length/2) - 1]);
+      middleArr.push(arr[(arr.length/2)]);
+    } else {
+      middleArr.push(arr[(Math.ceil(arr.length/2) - 1)]);
+    }
+    return middleArr;
+  }
+};
+
+//test arrays
+
+
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+
+// console.log(middle(smallArr));
+// console.log(middle(evenArr));
+// console.log(middle(oddArr));

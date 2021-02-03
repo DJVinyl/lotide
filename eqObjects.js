df54ab8ca60e990fdb981bc92d809123cd16d05d
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-02-03 22:04:41 +0000

    Equating Objects

---
+++ eqObjects.js
@@ -0,0 +1,59 @@
+const eqArrays = function(actual,expected) {
+  let equalCondition = true;
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        equalCondition = false;
+        break;
+      }
+    }
+  } else {
+    equalCondition = false;
+  }
+  return equalCondition;
+};
+
+
+const eqObjects = function(object1,object2) {
+  let equalCondition = true;
+  //This if statement may notbe neccessary due to eqArray function, however it is a quick check
+  if (Object.keys(object1).length === Object.keys(object2).length) { 
+    const keysObject1 = Object.keys(object1);
+    const keysObject2 = Object.keys(object2);
+    const keysBool = eqArrays(keysObject1,keysObject2);
+    if (keysBool === true)
+    {
+      //now compares the keys with values
+      for(let i = 0; i < keysObject1.length; i++)
+      {
+        let key = keysObject1[i];
+        let valueActual = object1[key]
+        let valueExpected = object2[key];
+        if (valueActual !== valueExpected){
+          equalCondition = false;
+        }
+      }
+    }
+  } else {
+   equalCondition = false;
+  }
+  return equalCondition;
+};
+
+
+//Tester calls
+
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// console.log(eqObjects(ab, ba)); // => true
+
+// const abc = { a: "1", b: "2", c: "3" };
+// console.log(eqObjects(ab, abc)); // => false
+
+
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// console.log(eqObjects(cd, dc)); // => true
+
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// console.log(eqObjects(cd, cd2)); // => false

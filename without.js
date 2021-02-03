a8b96efece92f1b3ec494d831c3a79b61c912578
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-02-03 01:12:55 +0000

    Removing elements within an Array

---
+++ without.js
@@ -0,0 +1,54 @@
+const assertArraysEqual = function(actual, expected) {
+  let equalCondition;
+  if (Array.isArray(actual) && Array.isArray(expected)) {
+    equalCondition = eqArrays(actual, expected);
+  } else {
+    equalCondition = false;
+  }
+
+  if (equalCondition === true) {
+    return console.log(`👏 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`👎 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actual,expected) {
+  let equalCondition = true;
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        equalCondition = false;
+        break;
+      }
+    }
+  } else {
+    equalCondition = false;
+  }
+  return equalCondition;
+};
+
+const without = function(source, itemsToRemove)
+{
+  let sourceCopy = source;
+  // console.log(sourceCopy);
+  // console.log(sourceCopy.slice(0))
+  for (let i = 0; i < sourceCopy.length; i++)
+  {
+    //console.log(sourceCopy[i]);
+    for(let j = 0; j< itemsToRemove.length; j++)
+    {
+      //console.log(itemsToRemove[j])
+      if (sourceCopy[i] === itemsToRemove[j])
+      {
+        console.log(`Im splicing i: ${i} j:${j}`)
+        sourceCopy.splice(i,1);
+      }
+    }
+  }
+
+  return sourceCopy;
+}
+
+console.log(without([1, 2, 3], [1]) )// => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
